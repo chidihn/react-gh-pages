06875af4b7e8db8c3bebbc6df0a009955ee35b86
Bùi Đình Chí <chi.bui@invigroup.com>
2019-03-25 21:11:35 +0700

    Updates

---
+++ precache-manifest.eaaf2d24ffa21f68c9d34b1a387f190e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  },
+  {
+    "revision": "ff7c4984d1d645135998",
+    "url": "/react-gh-pages/static/js/main.d7043063.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/react-gh-pages/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "ff7c4984d1d645135998",
+    "url": "/react-gh-pages/static/css/main.da5b993c.chunk.css"
+  },
+  {
+    "revision": "af26d95d975d43f8abd2d224870f9f29",
+    "url": "/react-gh-pages/index.html"
+  }
+];
